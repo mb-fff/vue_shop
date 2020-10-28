ea5218188934c77efaa09abc33af30553c6bf994
meibo <mb_fff@163.com>
2020-10-28 16:17:48 +0800

    完成了登陆功能

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-button @click="logout" type="info">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    logout() {
+      // 清空token
+      window.sessionStorage.clear()
+      // 跳转到登录页
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router.js'
+import './plugins/element.js'
+
+// 全局导入element-ui
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// Vue.use(ElementUI)
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 将axios挂载到Vue的原型对象上
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
